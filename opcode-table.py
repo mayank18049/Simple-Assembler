d31d75b40662ad0a10c563b96a14c8bf57204e6b
Mayank Rawat <43901354+mayank18049@users.noreply.github.com>
2021-07-27 23:39:12 +0530

    added inital ideas

--- opcode-table.py
+++ opcode-table.py
@@ -1,3 +1,9 @@
 # Dict about assembly opcode as key and machine opcode and other descriptions as values(type of instruction)
 # Provides functions to check if the provided assembly instrction is correct or not.
 # Calls error-gen if required 
+
+opcode_table = { 
+}
+
+def verify_instruction():
+    pass
